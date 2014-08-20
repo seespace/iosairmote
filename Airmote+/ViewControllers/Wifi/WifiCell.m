c60589d235bc52a34da6784757dc1d6b7580a216
Tuan Cao <caomanhtuanbsb@gmail.com>
2014-08-20 21:44:37 +0700

    Add bunch of view controllers

---
+++ Airmote+/ViewControllers/Wifi/WifiCell.m
@@ -0,0 +1,34 @@
+//
+//  WifiCell.m
+//  Airmote+
+//
+//  Created by Manh Tuan Cao on 8/20/14.
+//  Copyright (c) 2014 Long Nguyen. All rights reserved.
+//
+
+#import "WifiCell.h"
+
+@implementation WifiCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
