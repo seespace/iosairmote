3b3dc924a4ad8e72a358a71d9f0a693bb4f3de69
Tuan Cao <caomanhtuanbsb@gmail.com>
2014-08-20 14:37:26 +0700

    Add connected view.

--- Airmote+/ViewControllers/Wifi/InstructionViewController.m
+++ Airmote+/ViewControllers/Wifi/InstructionViewController.m
@@ -13,7 +13,11 @@
 @end
 
 @implementation InstructionViewController
-
+{
+    __weak IBOutlet UIView *connectedContainerView;
+    __weak IBOutlet UILabel *confirmationCodeLabel;
+    __weak IBOutlet UILabel *instructionLabel;
+}
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -27,6 +31,14 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    [UIView animateWithDuration:0.5 delay:0.2 options:0 animations:^{
+        instructionLabel.alpha = 0.0;
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.5 delay:0 options:0 animations:^{
+            connectedContainerView.alpha = 1.0;
+        } completion:NULL];
+    }];
 }
 
 - (void)didReceiveMemoryWarning
